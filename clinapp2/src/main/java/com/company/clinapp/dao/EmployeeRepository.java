e05b44842d8a148fa682a0476f5e64d502e524b8
Ajinkya Hatwar <ajinkya.hatwar@gmail.com>
2022-09-12 17:19:16 +0530

    project change with respect to datatype and few other changes

--- clinapp2/src/main/java/com/company/clinapp/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.clinapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.company.clinapp.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-}
